d6a22b09949014c5de5a0ef8a331f2ab2953e37c
Mikaela Duguil <kimmieduggz@gmail.com>
2021-03-29 17:27:43 -0700

    created submit listener to form

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 /* global data */
 /* exported data */
 
-var $imgUrl = document.querySelector('.img-url');
+var $form = document.querySelector('form');
 var $imgPreview = document.querySelector('.img-preview');
+// var $title = document.querySelector('.title');
+var $imgUrl = document.querySelector('.img-url');
+// var $notes = document.querySelector('.notes');
+// var $saveBtn = document.querySelector('.save');
 
 $imgUrl.addEventListener('input', function (event) {
   $imgPreview.setAttribute('src', event.target.value);
+});
+
+$form.addEventListener('submit', function (event) {
+  // var entry = {
+  //   title: $title.textContent,
+  //   imgUrl: $imgUrl.textContent,
+  //   notes: $notes.textContent
+  // };
 });
