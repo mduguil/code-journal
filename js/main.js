11a7eb085cc33e704cc9044417fabb0a51626cfc
Mikaela Duguil <kimmieduggz@gmail.com>
2021-03-29 17:01:58 -0700

    added event listener for img input that updates img src

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+
+var $imgUrl = document.querySelector('.img-url');
+var $imgPreview = document.querySelector('.img-preview');
+
+$imgUrl.addEventListener('input', function (event) {
+  $imgPreview.setAttribute('src', event.target.value);
+});
