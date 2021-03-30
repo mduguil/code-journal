99df0b584a9e25fc45cbacd7c5e6afaa0115d0a8
Mikaela Duguil <kimmieduggz@gmail.com>
2021-03-30 15:38:12 -0700

    loop thru entries and add to page

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ var defaultImg = $imgPreview.getAttribute('src');
 var $title = document.querySelector('#title');
 var $imgUrl = document.querySelector('#img-url');
 var $notes = document.querySelector('#notes');
+var $entryContainer = document.querySelector('.entry-container');
 
 $imgUrl.addEventListener('input', function (event) {
   $imgPreview.setAttribute('src', event.target.value);
@@ -28,37 +29,39 @@ $form.addEventListener('submit', function (event) {
   $form.reset();
 });
 
-// function addEntries(entry) {
-//   var $entryContainer = document.querySelector('.entry-container');
+function addEntries(entry) {
 
-//   var $ul = document.createElement('ul');
-//   $entryContainer.appendChild($ul);
+  var $ul = document.createElement('ul');
 
-//   var $containerRow = document.createElement('div');
-//   $containerRow.setAttribute('class', 'row');
-//   $ul.appendChild($containerRow);
+  var $containerRow = document.createElement('div');
+  $containerRow.setAttribute('class', 'row');
+  $ul.appendChild($containerRow);
 
-//   var $containerColumn1 = document.createElement('div');
-//   $containerColumn1.setAttribute('class', 'column-half');
-//   $containerRow.appendChild($containerColumn1);
+  var $containerColumn1 = document.createElement('div');
+  $containerColumn1.setAttribute('class', 'column-half');
+  $containerRow.appendChild($containerColumn1);
 
-//   var $entryImg = document.createElement('img');
-//   $entryImg.setAttribute('src', entry.imgUrl);
-//   $containerColumn1.appendChild($entryImg);
+  var $entryImg = document.createElement('img');
+  $entryImg.setAttribute('src', entry.imgUrl);
+  $containerColumn1.appendChild($entryImg);
 
-//   var $containerColumn2 = document.createElement('div');
-//   $containerColumn2.setAttribute('class', 'column-half');
-//   $containerRow.appendChild($containerColumn2);
+  var $containerColumn2 = document.createElement('div');
+  $containerColumn2.setAttribute('class', 'column-half');
+  $containerRow.appendChild($containerColumn2);
 
-//   var $entryTitle = document.createElement('li');
-//   $entryTitle.setAttribute('class', 'entry-title');
-//   $entryTitle.textContent = entry.title;
-//   $containerColumn2.appendChild($entryTitle);
+  var $entryTitle = document.createElement('li');
+  $entryTitle.setAttribute('class', 'entry-title');
+  $entryTitle.textContent = entry.title;
+  $containerColumn2.appendChild($entryTitle);
 
-//   var $entryNotes = document.createElement('li');
-//   $entryNotes.setAttribute('class', 'entry-notes');
-//   $entryNotes.textContent = entry.notes;
-//   $containerColumn2.appendChild($entryNotes);
+  var $entryNotes = document.createElement('li');
+  $entryNotes.setAttribute('class', 'entry-notes');
+  $entryNotes.textContent = entry.notes;
+  $containerColumn2.appendChild($entryNotes);
 
-//   return $ul;
-// }
+  return $ul;
+}
+
+for (var i = 0; i < data.entries.length; i++) {
+  $entryContainer.appendChild(addEntries(data.entries[i]));
+}
